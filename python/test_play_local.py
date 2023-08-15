f26ca8aa72576ae773341ff9a49013b2ae1f6255
Aakash Bhowmick <aakashbhowmick@gmail.com>
2023-08-15 10:44:53 +0530

    Improvement : Taking mean of several runs

--- python/test_play_local.py
+++ python/test_play_local.py
@@ -1,8 +1,10 @@
 import random
 
 from Hangman import HangmanAPI
+import numpy as np
 
-games_count = 50
+iterations = 2
+games_per_iteration = 4
 removal_percent = 0.6
 
 def remove_char_from_word(word, char_set):
@@ -18,54 +20,68 @@ if __name__ == "__main__":
     text_file.close()
 
     chosen_words_list = []
-    for i in range(games_count):
-        chosen_word = random.choice(full_dictionary)
-        chosen_words_list.append(chosen_word)
-        full_dictionary.remove(chosen_word)
+    for j in range(iterations):
+        chosen_words_for_iter = []
+        for i in range(games_per_iteration):
+            chosen_word = random.choice(full_dictionary)
+            chosen_words_for_iter.append(chosen_word)
+            full_dictionary.remove(chosen_word)
+        chosen_words_list.append(chosen_words_for_iter)
 
     test_list = []
-    for word in chosen_words_list:
-        chars_in_word = list(set(list(word)))
-        remove_count = int(len(chars_in_word) * removal_percent)
-        original_word = word
-        removed_char_set= set()
-        for j in range(remove_count):
-            char_to_remove = random.choice(chars_in_word)
-            word = word.replace(char_to_remove, "_")
-            chars_in_word.remove(char_to_remove)
-            removed_char_set.add(char_to_remove)
-        test_list.append((original_word, word, removed_char_set))
+    for word_list in chosen_words_list:
+        test_list_for_iter = []
+        for word in word_list:
+            chars_in_word = list(set(list(word)))
+            remove_count = int(len(chars_in_word) * removal_percent)
+            original_word = word
+            removed_char_set= set()
+            for j in range(remove_count):
+                char_to_remove = random.choice(chars_in_word)
+                word = word.replace(char_to_remove, "_")
+                chars_in_word.remove(char_to_remove)
+                removed_char_set.add(char_to_remove)
+            test_list_for_iter.append((original_word, word, removed_char_set))
+        test_list.append(test_list_for_iter)
 
     hangman = HangmanAPI(train_dict=full_dictionary)
-    success_count = 0
-    for test in test_list:
-        print(f"Starting test : {test}")
-        hangman.reset()
-        correct_word = test[0]
-        current_word = test[1]
-        remaining_chars = test[2]
+    success_rates = []
+    for itr in range(iterations):
+        tests_for_this_itr = test_list[itr]
+        success_count = 0
+        for test in tests_for_this_itr:
+            print(f"Starting iteration:{itr} test : {test}")
+            hangman.reset()
+            correct_word = test[0]
+            current_word = test[1]
+            remaining_chars = test[2]
 
-        success = False
-        for iattempt in range(6):
-            guess_char = hangman.guess(current_word)
-            hangman.guessed_letters.append(guess_char)
-            # Correct guess
-            if guess_char in remaining_chars:
-                remaining_chars.remove(guess_char)
-                current_word = remove_char_from_word(correct_word, remaining_chars)
-                if len(remaining_chars) == 0:
-                    success = True
-                    break
+            success = False
+            for iattempt in range(6):
+                guess_char = hangman.guess(current_word)
+                hangman.guessed_letters.append(guess_char)
+                # Correct guess
+                if guess_char in remaining_chars:
+                    remaining_chars.remove(guess_char)
+                    current_word = remove_char_from_word(correct_word, remaining_chars)
+                    if len(remaining_chars) == 0:
+                        success = True
+                        break
 
 
-        if success:
-            success_count += 1
-            print(f"**Successful**, correct_word:{correct_word}, final_word:{current_word}")
-        else:
-            print(f">> Failed, correct_word:{correct_word}, final_word:{current_word}")
+            if success:
+                success_count += 1
+                print(f"**Successful**, correct_word:{correct_word}, final_word:{current_word}")
+            else:
+                print(f">> Failed, correct_word:{correct_word}, final_word:{current_word}")
 
-    success_rate = success_count / len(test_list) * 100.0;
-    print(f"Success rate:{success_rate}")
+        success_rate = success_count / len(tests_for_this_itr) * 100.0
+        success_rates.append(success_rate)
+        print(f"Success rate iter {itr}:{success_rate}")
+
+    # Calculate mean success rate
+    mean_success_rate = np.mean(success_rates)
+    print(f"Mean Success Rate : {mean_success_rate}")
 
 
 
